92744929a0735354da57e5ef22eb29446bae136a
Seyedk-Gihtub-Repo <seyed.ketabchi@gmail.com>
2022-02-12 19:56:20 -0800

    making good changes

--- main.tf
+++ main.tf
@@ -4,36 +4,36 @@ module "aws_sls_model" {
 
   api_gateways = local.api_gateways
   functions    = local.functions
-  vpcs   = local.vpcs
+  vpcs         = local.vpcs
   #  static_websites        = local.static_websites
   #  dynamodb_tables        = local.ddb_tables
   global_settings = local.global_settings
 
-  
+
   # tfstates = try(var.serverless.tfstates,{})
 
   tags           = local.tags
   remote_objects = local.remote
 
   current_serverless_key = var.serverless.key
-  api_integrations           = try(local.api_integrations, {})
+  api_integrations       = try(local.api_integrations, {})
   client_config          = {}
 
-  cognito_userpools = local.cognito_userpools
+  cognito_userpools = try(local.cognito_userpools, {})
 
   # todo: the following line would add cloudfront and dynamodb tables
 
   # cloud_fronts = local.cloud_front_distributions
   # todo: the following line would add cloudfront and dynamodb tables
-   dynamodb_tables = local.dynamodb_tables
-   step_functions = local.step_functions
+  dynamodb_tables = local.dynamodb_tables
+  step_functions  = local.step_functions
 
-   acms = local.acms
-   s3_buckets = local.s3_buckets
-   lambda_layers = local.lambda_layers
- 
+  acms          = local.acms
+  s3_buckets    = local.s3_buckets
+  lambda_layers = local.lambda_layers
+
   # s3_bucket_objects = local.s3_bucket_objects
- 
+
 }
 
 output "objects" {
